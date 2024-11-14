5403a65bcb96e061dfcdccbb133453c5868134a4
sp8783 <p.sp8783@gmail.com>
2024-11-14 13:50:53 +0900

    Refactor JSX to directly map and render memo items

--- src/MemoList.js
+++ src/MemoList.js
@@ -1,13 +1,11 @@
 export default function MemoList({ memos, onSelectMemo }) {
-  const memoList = memos.map((memo) => (
-    <li key={memo.id} onClick={() => onSelectMemo(memo.id)}>
-      {memo.title}
-    </li>
-  ));
-
   return (
     <div>
-      <ul>{memoList}</ul>
+      {memos.map((memo) => (
+        <li key={memo.id} onClick={() => onSelectMemo(memo.id)}>
+          {memo.title}
+        </li>
+      ))}
     </div>
   );
 }
